9567daba6f22d6bad31755c5bb433ed3cd18067a
Nikita Borisov <nborisov256@gmail.com>
2021-12-19 17:24:54 +0300

    + Valid Parentheses

---
+++ Valid Parentheses/isValid.js
@@ -0,0 +1,21 @@
+function isValid(s) {
+    let brackets = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+        },
+        temp = [];
+
+    for (let i = 0; i < s.length; i++) {
+    if (isClosedBracket(s[i])) {
+        if (brackets[s[i]] !== temp.pop()) return false;
+    } else {
+        temp.push(s[i]);
+    }
+    }
+    return temp.length === 0;
+}
+
+function isClosedBracket(check) {
+    return [")", "]", "}"].indexOf(check) > -1;
+}
